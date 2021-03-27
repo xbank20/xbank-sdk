16e44238728bf81d27046ca32e0132a442c066db
free2swap <ddcommune@gmail.com>
2021-03-27 10:21:54 +0800

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x3D9f48692fF5B93b884cA7F4AAaC7Cc7b598d71C'
+
+export const INIT_CODE_HASH = 'b51327b9c1a61dd4e909bef88e30d7f08644e2b161057e13cd925f1b0b6a61ec'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
